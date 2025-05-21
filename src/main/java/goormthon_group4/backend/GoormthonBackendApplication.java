e8db8cbf91d421340b42ecd462766e7f399bc98c
newrhyme <ksh000624@naver.com>
2025-05-21 17:21:51 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/main/java/goormthon_group4/backend/GoormthonBackendApplication.java
@@ -0,0 +1,13 @@
+package goormthon_group4.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormthonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormthonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goormthon_group4/backend/GoormthonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package goormthon_group4.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormthonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
