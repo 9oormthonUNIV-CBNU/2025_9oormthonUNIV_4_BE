7f888c5ce8344641c438932a3619bbb307791b14
newrhyme <ksh000624@naver.com>
2025-05-21 21:42:43 +0900

    feat : 카카오 로그인 기능 구현

---
+++ src/main/java/goormthon_group4/backend/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package goormthon_group4.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/oauth/**", "/kakao-login-button.png").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉션 페이지 "/" -> 다른걸로
+                );
+        return http.build();
+    }
+}
