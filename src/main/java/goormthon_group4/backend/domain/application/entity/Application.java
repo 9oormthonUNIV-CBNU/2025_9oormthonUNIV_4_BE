74bbe6a4ca3defcb15841a3951b44c8430f6031f
NamMinwu <minwoo000625@naver.com>
2025-05-23 18:31:02 +0900

    feat : applicaiton, team, teamInfo entity 생성

---
+++ src/main/java/goormthon_group4/backend/domain/application/entity/Application.java
@@ -0,0 +1,70 @@
+package goormthon_group4.backend.domain.application.entity;
+
+import goormthon_group4.backend.domain.team.entity.Team;
+import goormthon_group4.backend.domain.user.entity.User;
+import goormthon_group4.backend.global.common.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Application extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  private ApplicationStatus status;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id", nullable = false)
+  private Team team;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String phoneNumber;
+
+  @Column(nullable = false)
+  private String introduce;
+
+  @Column(nullable = false)
+  private String purpose;
+
+  @Column(nullable = false)
+  private String skillExperience;
+
+  @Column(nullable = false)
+  private String strengthsExperience;
+
+  @Column(nullable = true)
+  private String fileUrl; // nullable
+
+  @Column(nullable = true)
+  private String additionalInfo; // nullable
+
+}
