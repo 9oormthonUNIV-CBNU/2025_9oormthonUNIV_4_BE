c5119ec7029122b0d422eb502305b449cdd388a0
Juhyun Park <pjh33188@chungbuk.ac.kr>
2025-05-24 14:54:49 +0900

    feat: add Member, Notify, ToolLink entities and update Team entity

---
+++ src/main/java/goormthon_group4/backend/domain/team/entity/Notify.java
@@ -0,0 +1,33 @@
+package goormthon_group4.backend.domain.team.entity;
+
+import goormthon_group4.backend.domain.user.entity.User;
+import goormthon_group4.backend.global.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "notify")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Notify extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
