89932b5b2c1145a05bacf19ed4c6e7dec856e429
newrhyme <ksh000624@naver.com>
2025-05-27 03:31:38 +0900

    chore : 대학인증 api UnivCert 위한 작업

---
+++ src/main/java/goormthon_group4/backend/domain/univcert/dto/UnivCodeRequestDto.java
@@ -0,0 +1,16 @@
+package goormthon_group4.backend.domain.univcert.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UnivCodeRequestDto {
+    private String univName;
+    private String univCode;
+    private int code;
+}
