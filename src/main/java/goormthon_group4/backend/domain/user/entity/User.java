2df10033a9850cdf81c80a42e00399842e7ed134
newrhyme <ksh000624@naver.com>
2025-05-27 03:44:21 +0900

    fix : 충돌 해결

--- src/main/java/goormthon_group4/backend/domain/user/entity/User.java
+++ src/main/java/goormthon_group4/backend/domain/user/entity/User.java
@@ -1,57 +1,45 @@
 package goormthon_group4.backend.domain.user.entity;
 
-import goormthon_group4.backend.domain.application.entity.Application;
-import goormthon_group4.backend.domain.team.entity.Team;
-import goormthon_group4.backend.domain.user.entity.Role;
-import goormthon_group4.backend.domain.user.entity.UserInfo;
-import goormthon_group4.backend.global.common.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class User extends BaseEntity {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String socialId;
-
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Enumerated(EnumType.STRING)
-    @Builder.Default
-    private Role role = Role.USER;
-
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
-
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    // 단방향 매핑 (User가 삭제되면 UserInfo도 삭제됨)
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_info_id")
     private UserInfo userInfo;
 
-    @OneToMany(mappedBy = "leader", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<Team> leadingTeams = new ArrayList<>();
 
-    public void addTeam(Team team) {
-        leadingTeams.add(team);
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
     }
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Application> applications = new ArrayList<>();
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
-    public void addApplication(Application application) {
-        applications.add(application);
+    public UserInfo getUserInfo() {
+        return userInfo;
     }
 }
+
