f2dd4ab19c3c67c300cd8cfcd6a1adff6cc4d905
NamMinwu <minwoo000625@naver.com>
2025-05-26 16:40:33 +0900

    feat : s3 업로드 기능추가

---
+++ src/main/java/goormthon_group4/backend/global/config/S3Config.java
@@ -0,0 +1,52 @@
+package goormthon_group4.backend.global.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+
+@Configuration
+public class S3Config {
+  @Value("${cloud.aws.credentials.access-key}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secret-key}")
+  private String secretKey;
+
+  @Value("${cloud.aws.region.static}")
+  private String region;
+
+  @Bean
+  public S3Client s3Client() {
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+    return S3Client.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(credentials))
+        .build();
+  }
+
+  @Bean
+  public S3AsyncClient s3AsyncClient() {
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+    return S3AsyncClient.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(credentials))
+        .build();
+  }
+
+  @Bean
+  public S3TransferManager transferManager(S3AsyncClient s3Client) {
+    return S3TransferManager.builder()
+        .s3Client(s3Client)
+        .build();
+  }
+
+}
