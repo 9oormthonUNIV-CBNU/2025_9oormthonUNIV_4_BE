56e2dc1daac9a0bdb6ed1804e82c5b6bfbf86242
newrhyme <ksh000624@naver.com>
2025-05-26 11:34:28 +0900

    feat : jwt 토큰 ➡️ 쿠키

--- src/main/java/goormthon_group4/backend/domain/post/controller/auth/controller/TestController.java
+++ src/main/java/goormthon_group4/backend/domain/post/controller/auth/controller/TestController.java
@@ -1,13 +1,22 @@
 package goormthon_group4.backend.domain.post.controller.auth.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
-    @GetMapping("/api/test/protected")
+    @GetMapping("/protected")
     public String protectedEndpoint() {
         return "✅ 토큰이 유효하므로 접근 성공!";
+    }
+
+    @GetMapping("/cookie")
+    public ResponseEntity<String> cookieEndpoint(@CookieValue("token") String jwttoken) {
+        return ResponseEntity.ok("JWT 쿠키 인증 성공" + jwttoken);
     }
 
 }
