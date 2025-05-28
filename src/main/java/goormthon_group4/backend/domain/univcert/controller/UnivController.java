f6cc34523e19470a71a59c905c000d38f3dd95ae
newrhyme <ksh000624@naver.com>
2025-05-29 03:49:17 +0900

    feat : UnivCertAPI에 Swagger 문서 공통 응압 포맷 적용

--- src/main/java/goormthon_group4/backend/domain/univcert/controller/UnivController.java
+++ src/main/java/goormthon_group4/backend/domain/univcert/controller/UnivController.java
@@ -3,8 +3,8 @@ package goormthon_group4.backend.domain.univcert.controller;
 import goormthon_group4.backend.domain.univcert.dto.UnivCodeRequestDto;
 import goormthon_group4.backend.domain.univcert.dto.UnivRequestDto;
 import goormthon_group4.backend.domain.univcert.service.UnivService;
-import goormthon_group4.backend.domain.user.entity.User;
 import goormthon_group4.backend.global.auth.CustomUserDetails;
+import goormthon_group4.backend.global.common.exception.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -17,28 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@Tag(name = "대학교 인증 API", description = "UnivCertAPI를 통해 이메일 인증")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/univcert")
-
 public class UnivController {
+
     private final UnivService univService;
 
+    @Operation(summary = "대학교 이메일 인증 요청", description = "유저 학교 이메일로 인증 코드 전송")
     @PostMapping
-    public ResponseEntity<String> univCertify(@RequestBody UnivRequestDto univRequestDto,
-                                              @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
-        return ResponseEntity.ok(univService.univCertify(univRequestDto, customUserDetails));
+    public ApiResponse<String> univCertify(@RequestBody UnivRequestDto univRequestDto,
+                                           @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
+        String result = univService.univCertify(univRequestDto, customUserDetails);
+        return ApiResponse.success(result);
     }
 
+    @Operation(summary = "인증 코드 검증", description = "사용자가 받은 인증 코드 검증")
     @PostMapping("/code")
-    public ResponseEntity<String> univCertifyCode(@RequestBody UnivCodeRequestDto univCodeRequestDto,
-                                                  @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
-        return ResponseEntity.ok(univService.univCertifyCode(univCodeRequestDto, customUserDetails));
+    public ApiResponse<String> univCertifyCode(@RequestBody UnivCodeRequestDto univCodeRequestDto,
+                                               @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
+        String result = univService.univCertifyCode(univCodeRequestDto, customUserDetails);
+        return ApiResponse.success(result);
     }
 
+    @Operation(summary = "인증 정보 초기화", description = "학교 인증 관련 정보 초기화")
     @PostMapping("/clear")
-    public ResponseEntity<String> univClear(@RequestBody UnivRequestDto univRequestDto,
-                                            @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
-        return ResponseEntity.ok(univService.univClear(univRequestDto, customUserDetails));
+    public ApiResponse<String> univClear(@RequestBody UnivRequestDto univRequestDto,
+                                         @AuthenticationPrincipal CustomUserDetails customUserDetails) throws IOException {
+        String result = univService.univClear(univRequestDto, customUserDetails);
+        return ApiResponse.success(result);
     }
 }
--- src/main/java/goormthon_group4/backend/global/oauth/JwtTokenFilter.java
+++ src/main/java/goormthon_group4/backend/global/oauth/JwtTokenFilter.java
@@ -15,17 +15,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class JwtTokenFilter extends GenericFilterBean {
